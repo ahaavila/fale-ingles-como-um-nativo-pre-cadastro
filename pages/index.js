bde653bd19dd6c602ddb8cd42831fe616846e377
Augusto Ávila <augusto.avila@ctw.bmwgroup.com>
2023-01-13 19:03:19 +0000

    feat: add logics to send mail  and validations

--- pages/index.js
+++ pages/index.js
@@ -1,111 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
-import logo from '../public/assets/images/logo-azul.png';
 import teste from '../public/assets/images/teste.png';
 import styles from '../styles/Home.module.css'
 import Link from "next/link";
 
 export default function Home() {
-
-  // const [snackbarState, setSnackbarState] = useState({
-  //   open: false,
-  //   vertical: 'top',
-  //   horizontal: 'center',
-  // });
-  //
-  // const { vertical, horizontal, open } = snackbarState;
-
-  // const sendMail = async() => {
-  //   setIsSendingEmail(true);
-  //   try {
-  //     await axios.post('https://api.emailjs.com/api/v1.0/email/send', {
-  //       user_id: 'user_YWHRL5p0UldXqQkZxq8QE',
-  //       service_id: 'service_gxkb4se',
-  //       template_id: 'template_gtqt9vr',
-  //       template_params: {
-  //         name,
-  //         email,
-  //       }
-  //     });
-  //
-  //     const response = await axios.post('https://fale-ingles-como-um-nativo-pre-cadastro.vercel.app/api/user', {
-  //       name,
-  //       email,
-  //     });
-  //
-  //     console.log('response', response);
-  //
-  //     setSnackbarState({ open: true, vertical: 'top', horizontal: 'right' });
-  //     setIsError(false);
-  //     setShowAlert(true);
-  //     setMessage('Email enviado com sucesso!');
-  //   } catch (err) {
-  //     console.error(err);
-  //     setSnackbarState({ open: true, vertical: 'top', horizontal: 'right' });
-  //     setIsError(true);
-  //     setShowAlert(true);
-  //     setMessage('Falha ao enviar o email');
-  //   } finally {
-  //     setIsSendingEmail(false);
-  //   }
-  // }
-
-  // const closeAlert = () => {
-  //   setMessage('');
-  //   setShowAlert(false);
-  // }
-
-  // function validateEmail(email) {
-  //   var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-  //   if (email?.length === undefined) return false;
-  //   if (reg.test(email)) {
-  //     return false;
-  //   }
-  //   else {
-  //     return true;
-  //   }
-  // }
-  //
-  // function validateName(validName) {
-  //   if (validName?.length === 0) return false;
-  //   if (validName?.length < 3) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
-
-  // const handleClose = (event, reason) => {
-  //   if (reason === 'clickaway') {
-  //     return;
-  //   }
-  //
-  //   setSnackbarState({ ...snackbarState, open: false });
-  // };
-
-  // const theme = createTheme({
-  //   components: {
-  //     // Name of the component
-  //     MuiGrid: {
-  //       styleOverrides: {
-  //         // Name of the slot
-  //         root: {
-  //           // Some CSS
-  //           padding: '6rem'
-  //         },
-  //       },
-  //     },
-  //     MuiButton: {
-  //       styleOverrides: {
-  //         root: {
-  //           height: '4rem',
-  //           fontSize: '1rem'
-  //         }
-  //       }
-  //     }
-  //   },
-  // });
 
   return (
     <div className={styles.container}>
@@ -115,135 +14,58 @@ export default function Home() {
 
       <div className={styles.title}>
         <div className={styles.teste}>
-          <Image src={logo} alt="Logo Lorenabandaosoueu" width={350} height={180}/>
           <h1 className={styles.h1}>
-            <span className={styles.aulao}>Aulão do</span> <span className={styles.get}></span>GET
+              <p>Bem-vindo ao</p>
+            <span className={styles.aulao}>Aulão do</span> <span className={styles.get}></span>GET!
           </h1>
-          <Image
-            src={teste}
-            alt="Estude inglês"
-            className={styles.englishImage}
-            // layout="responsive"
-            width={300}
-            height={300}
-          />
         </div>
       </div>
       <div className={styles.form}>
+          <h1>Aulão do GET</h1>
+          <p><i>Se deseja avançar no inglês, está no lugar certo!</i></p>
+          <p className={styles.textBreak}>
+              O verbo GET é amplamente usado pelos nativos no <br />
+              dia-a-dia, com diversos significados. E neste AULÃO eu <br />
+              irei abordar estas diferenças e muito mais!
+          </p>
+          <h1>Sobre o Aulão:</h1>
+          <ul className={styles.formUl}>
+              <li>Acontecerá dia 25/01/2023, 19 horas (horário de Brasília);</li>
+              <li>Pelo ZOOM;</li>
+              <li>Com duração de 2 horas (um intervalo de 15 minutos);</li>
+              <li>Material de apoio e lista de exercícios enviados para o email cadastrado, antes do AULÃO;</li>
+              <li>Valor: 27,00.</li>
+          </ul>
+
+          <div className={styles.duvida}>
+              <h3>Dúvida sobre o Aulão</h3>
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              <p>"- Lorena! Sou iniciante, o AULÃO é para mim?"</p>
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              <p>"- Sim!!! O AULÃO é para todos os níveis. Porque eu acredito que desde o <br />
+                  início do aprendizado, o aluno precisa aprender a se comunicar como um nativo. <br />
+              </p>
+          </div>
+
+
           <Link href="/payment">
-              <button>Fazer Inscrição</button>
+              <button className={styles.paymentButton}>
+                  QUERO ME INSCREVER!
+                  <span className="material-symbols-outlined">
+                    arrow_forward
+                  </span>
+              </button>
           </Link>
       </div>
-      {/*<ThemeProvider theme={theme}>*/}
-      {/*  {*/}
-      {/*    showAlert &&*/}
-      {/*    (*/}
-      {/*      isError ? */}
-      {/*      <Snackbar */}
-      {/*        anchorOrigin={{ vertical, horizontal }}*/}
-      {/*        open={open} */}
-      {/*        autoHideDuration={3000} */}
-      {/*        onClose={handleClose}*/}
-      {/*        key={vertical + horizontal}*/}
-      {/*      >*/}
-      {/*        <Alert onClose={() => closeAlert()} variant="filled" severity="error">{ message }</Alert>*/}
-      {/*      </Snackbar> : */}
-      {/*      <Snackbar */}
-      {/*        anchorOrigin={{ vertical, horizontal }}*/}
-      {/*        open={open} */}
-      {/*        autoHideDuration={3000} */}
-      {/*        onClose={handleClose}*/}
-      {/*        key={vertical + horizontal}*/}
-      {/*      >*/}
-      {/*        <Alert onClose={() => closeAlert()} variant="filled" severity="success">{ message }</Alert>*/}
-      {/*      </Snackbar>*/}
-      {/*    )*/}
-      {/*  }*/}
-      {/*    <div className={styles.title}>*/}
-      {/*      <div className={styles.teste}>*/}
-      {/*        <Image components={logo} alt="Logo Lorenabandaosoueu" id={styles.testeImg} width={350} height={180}/>*/}
-      {/*        <h1>*/}
-      {/*          LISTA DE ESPERA PARA CURSO FALE INGLÊS COMO UM NATIVO, COM LORENA BRANDÃO*/}
-      {/*        </h1>*/}
-      {/*        <Image */}
-      {/*          components={teste} */}
-      {/*          alt="Estude inglês" */}
-      {/*          id={styles.englishImage}*/}
-      {/*          // layout="responsive"*/}
-      {/*          width={500}*/}
-      {/*          height={500}*/}
-      {/*        />*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*    <div className={styles.formulario}>*/}
-      {/*      <h2>*/}
-      {/*        CADASTRE-SE ABAIXO PARA ENTRAR NA LISTA DE ESPERA E RECEBER INFORMAÇÕES SOBRE O CURSO:*/}
-      {/*      </h2>*/}
-      {/*      <Box*/}
-      {/*        component="form"*/}
-      {/*        method='POST'*/}
-      {/*        sx={{*/}
-      {/*          '& > :not(style)': { m: 1, width: '100%' },*/}
-      {/*          '@media (min-width: 601px)': { width: '70%' },*/}
-      {/*        }}*/}
-      {/*        noValidate*/}
-      {/*        autoComplete="off"*/}
-      {/*        display="flex"*/}
-      {/*        flexDirection="column"*/}
-      {/*        justifyContent="center"*/}
-      {/*        alignContent="center"*/}
-      {/*        alignItems="center"*/}
-      {/*        padding="2rem"*/}
-      {/*        margin="0 auto"*/}
-      {/*      >*/}
-      {/*          <TextField */}
-      {/*            id="name"*/}
-      {/*            className="inputs"*/}
-      {/*            label="Como você gostaria de ser chamado?"*/}
-      {/*            variant="outlined" */}
-      {/*            color='success'*/}
-      {/*            value={name}*/}
-      {/*            onChange={e => setName(e.target.value)}*/}
-      {/*            error={validateName(name)}*/}
-      {/*            helperText={validateName(name) && 'O nome deve conter no mínimo 3 e no máximo 15 caracteres e somente letras.'}*/}
-      {/*            required*/}
-      {/*            fullWidth*/}
-      {/*            sx={{*/}
-      {/*              '& > :not(style)': { fontSize: '1.5rem' }*/}
-      {/*            }}*/}
-      {/*          />*/}
-      {/*        <TextField */}
-      {/*          id="email"*/}
-      {/*          className="inputs" */}
-      {/*          label="Deixe seu melhor e-mail."*/}
-      {/*          variant="outlined"*/}
-      {/*          color='success'*/}
-      {/*          type="email" */}
-      {/*          value={email}*/}
-      {/*          onChange={e => setEmail(e.target.value)}*/}
-      {/*          error={validateEmail(email)}*/}
-      {/*          helperText={validateEmail(email) && 'E-mail inválido!'}*/}
-      {/*          required */}
-      {/*          fullWidth*/}
-      {/*          sx={{*/}
-      {/*            '& > :not(style)': { fontSize: '1.5rem' }*/}
-      {/*          }}*/}
-      {/*        />*/}
-      {/*        <Button */}
-      {/*          className="inputs"*/}
-      {/*          variant="contained" */}
-      {/*          color="success"*/}
-      {/*          onClick={() => sendMail()}*/}
-      {/*          disabled={(name?.length > 2) && (!validateEmail(email)) ? false : true}*/}
-      {/*          sx={{*/}
-      {/*            '& > :not(style)': { fontSize: '2rem', height: '2rem' }*/}
-      {/*          }}*/}
-      {/*        >*/}
-      {/*          { isSendingEmail ? 'CADASTRANDO...' : 'QUERO ENTRAR PARA A LISTA DE ESPERA' }*/}
-      {/*        </Button>*/}
-      {/*      </Box>*/}
-      {/*    </div>*/}
-      {/*</ThemeProvider>*/}
+        <div className={styles.image}>
+            <Image
+                src={teste}
+                alt="Estude inglês"
+                className={styles.englishImage}
+                width={300}
+                height={300}
+            />
+        </div>
     </div>
   )
 }
--- pages/payment/index.js
+++ pages/payment/index.js
@@ -1,7 +1,37 @@
-import React from 'react';
-import styles from './payment.module.css'
-import Link from "next/link";
+import React, { useState } from 'react';
+import styles from './payment.module.css';
+import {sendMail, validateEmail, validateName} from "../../utils/mail";
+import { useRouter } from 'next/router'
 export default function Payment() {
+    const [name, setName] = useState('');
+    const [nameError, setNameError] = useState('');
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [phone, setPhone] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    const router = useRouter();
+
+    function mailValidate(email) {
+        validateEmail(email) ? setEmailError('Email inválido') : setEmailError('');
+    }
+
+    function nameValidate(name) {
+        validateName(name) ? setNameError('Nome tem que ter mais de 3 caracteres') : setNameError('');
+    }
+
+    async function mail(name, email,  phone)  {
+        setLoading(true);
+        const response = await sendMail(name, email,  phone);
+        if (response.status === 200)  {
+            setLoading(false);
+            await router.push('/thanks');
+        } else {
+            setError(response.error);
+        }
+    }
+
     return (
         <>
         <header className={styles.header}>
@@ -15,21 +45,36 @@ export default function Payment() {
         </header>
           <div className={styles.container}>
             <div className={styles.cadastro}>
-                <h3>Passo 1: Dados para cadastro</h3>
+                <h2>Passo 1: Dados para cadastro</h2>
                 <div className={styles.cadastroFormDiv}>
                     <form className={styles.cadastroForm}>
                         <label>Nome</label>
-                        <input  required/>
+                        <input
+                            name="name"
+                            type="text"
+                            onChange={e => setName(e.target.value)}
+                            onBlur={e => nameValidate(e.target.value)}
+                            required
+                        />
+                        <small style={{ color: 'red' }}>{nameError}</small>
                         <label>Email para receber o material</label>
-                        <input />
+                        <input
+                            name="email"
+                            type="email"
+                            onChange={e => setEmail(e.target.value)}
+                            onBlur={e => mailValidate(e.target.value)}
+                            required
+                        />
+                        <small style={{ color: 'red' }}>{emailError}</small>
                         <label>Whatsapp</label>
-                        <input />
+                        <input name="phone" type="tel" onChange={e => setPhone(e.target.value)} />
+                        <small style={{ color: 'red' }}>{error}</small>
                     </form>
                 </div>
             </div>
 
             <div className={styles.payment}>
-                <h3>Passo 2: Pagamento</h3>
+                <h2>Passo 2: Pagamento</h2>
                 <div className={styles.paymentContent}>
                     <h4>Transferência:</h4>
                     <div>
@@ -50,14 +95,17 @@ export default function Payment() {
             </div>
 
             <div className={styles.info}>
-                <h3>Passo 3: Considerações Finais</h3>
+                <h2>Passo 3: Considerações Finais</h2>
                 <div className={styles.infoContent}>
                     <p>Para que possamos confirmar sua inscrição, você deverá clicar no botão <strong>Finalizar Inscrição</strong></p>
                     <p>e enviar o comprovante do pagamento para o email: <strong>lorenabrandaosoueu@gmail.com</strong></p>
-
-                    <Link href="/thanks">
-                        <button className={styles.infoButton}>Finalizar Inscrição</button>
-                    </Link>
+                    <p>com o email que foi feita a inscrição.</p>
+                    <button
+                        className={styles.infoButton}
+                        onClick={() => mail(name, email, phone)}
+                    >
+                        {loading ? 'Enviando...' : 'Finalizar Inscrição'}
+                    </button>
                 </div>
             </div>
           </div>
